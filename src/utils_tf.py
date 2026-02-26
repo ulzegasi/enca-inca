211c332f9ca0bec9bada2dc1584a3f934a16063b
Simone Ulzega <ulzg@zhaw.ch>
2026-02-26 17:13:38 +0100

    minor changes to utils_tf.py

--- src/utils_tf.py
+++ src/utils_tf.py
@@ -1,9 +1,8 @@
 #Author: Firat Ozdemir, October 2019, firat.ozdemir@datascience.ch
 import tensorflow as tf
-from tensorflow.python.ops import math_ops
+# from tensorflow.python.ops import math_ops
 
 class LearningRateScheduleExponentialDecayWithLinearWarmup(tf.keras.optimizers.schedules.LearningRateSchedule):
-    @tf.function
     def __init__(self, steps_warmup, initial_learning_rate, decay_steps, decay_rate, staircase):
         super(LearningRateScheduleExponentialDecayWithLinearWarmup, self).__init__()
         self.steps_warmup = steps_warmup
@@ -13,7 +12,7 @@ class LearningRateScheduleExponentialDecayWithLinearWarmup(tf.keras.optimizers.s
         self.lr_expdecay = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=initial_learning_rate, decay_steps=decay_steps, decay_rate=decay_rate, staircase=staircase)
     @tf.function
     def linear_warmup(self, step):
-        return self.slope * (math_ops.cast(step, tf.float32)+1.) + self.warmup_start_lr
+        return self.slope * (tf.cast(step, tf.float32) + 1.0) + self.warmup_start_lr
     @tf.function
     def __call__(self, step):
         if step < self.steps_warmup:
